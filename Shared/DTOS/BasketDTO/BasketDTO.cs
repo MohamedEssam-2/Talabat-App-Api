2e8374f4729e4c744ebf3953426e972b33d77e5a
mohamed essam <mido8786essam4@gmail.com>
2026-01-20 23:46:06 +0200

    Talabat-App-Project

--- Shared/DTOS/BasketDTO/BasketDTO.cs
+++ Shared/DTOS/BasketDTO/BasketDTO.cs
@@ -10,5 +10,9 @@ namespace Shared.DTOS.BasketDTO
     {
         public string Id { get; set; }
         public ICollection<BasketItemDTO> Items { get; set; }
+        public string? ClientSecret { get; set; }
+        public string? PaymentIntentId { get; set; }
+        public int? DeliveryMethodId { get; set; }
+        public decimal? ShippingPrice { get; set; }
     }
 }
